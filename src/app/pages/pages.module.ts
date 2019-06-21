ac052c355ca781fbc87fc391bd1c366ecd3f52cc
Ronald Castro <ronaldcastro.22@hotmail.com>
2019-06-20 21:58:44 -0500

    Input y Output

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // Rutas
 import { PAGES_ROUTE } from './pages.routing';
 
 // Módulos
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { ChartsModule } from 'ng2-charts';
 
 // Componentes
 import { PagesComponent } from './pages.component';
@@ -12,12 +15,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficaDonaComponent } from '../components/grafica-dona/grafica-dona.component';
+
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficaDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -25,8 +33,11 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         Graficas1Component
     ],
     imports: [
+        BrowserModule,
         SharedModule,
-        PAGES_ROUTE
+        PAGES_ROUTE,
+        FormsModule,
+        ChartsModule
     ]
 })
 
